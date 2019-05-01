f17d7c12fe9a5f69ff5e99c60d2f4bb0751ae25d
Milana Campbell <milana.campbell@gmail.com>
2019-05-01 16:16:05 -0400

    2nd commit and correct 2nd instance assignment

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -12,22 +12,28 @@ public class Car {
     private String make;
     private String starting;
     private String accelerating;
-    private String stopping;
+    private String stopped;
     private String mph;
+    private String passes;
+    private String passed;
 
-    //default constructor
+    //in IntelliJ right click and select Generator, select none to create default constructor
     public Car() {
     }
 
-    //overloaded constructor
-    public Car(String color, String make, String starting, String accelerating, String stopping, String mph) {
+    public Car(String color, String make, String starting, String accelerating, String stopped, String mph, String passes, String passed) {
         this.color = color;
         this.make = make;
         this.starting = starting;
         this.accelerating = accelerating;
-        this.stopping = stopping;
+        this.stopped = stopped;
         this.mph = mph;
+        this.passes = passes;
+        this.passed = passed;
     }
+
+    //code must be between last curly brace
+
 
     public String getColor() {
         return color;
@@ -46,7 +52,7 @@ public class Car {
     }
 
     public String getStarting() {
-        return "The " + getColor() + " " + getMake() + " is starting.";
+        return starting;
     }
 
     public void setStarting(String starting) {
@@ -54,19 +60,19 @@ public class Car {
     }
 
     public String getAccelerating() {
-        return "The " + getColor() + " " + getMake() + " is accelerating.";
+        return accelerating;
     }
 
     public void setAccelerating(String accelerating) {
         this.accelerating = accelerating;
     }
 
-    public String getStopping() {
-        return stopping;
+    public String getStopped() {
+        return stopped;
     }
 
-    public void setStopping(String stopping) {
-        this.stopping = stopping;
+    public void setStopped(String stopped) {
+        this.stopped = stopped;
     }
 
     public String getMph() {
@@ -77,5 +83,50 @@ public class Car {
         this.mph = mph;
     }
 
+    public String getPasses() {
+        return passes;
+    }
 
-}
+    public void setPasses(String passes) {
+        this.passes = passes;
+    }
+
+    public String getPassed() {
+        return passed;
+    }
+
+    public void setPassed(String passed) {
+        this.passed = passed;
+    }
+
+    //custom methods I created
+    public String isStarting () {
+        return "The " + getColor() + " " + getMake() + " is starting";
+    }
+
+    public String isAccelerating (){
+    return "The " + getColor() + " " + getMake() + " is accelerating";
+        }
+    public String isStopped (){
+     return "The " + getColor() + " " + getMake() + " has stopped to have its emissions checked";
+
+    }
+    public String isPasses (){
+        return "The " + getColor() + " " + getMake() + " passes its emissions test";
+    }
+    public String isMph (){
+        return "The " + getColor() + " " + getMake() + " is going 50 Mph";
+    }
+    public String isNotMoving () {
+        return "The " + getColor() + " " + getMake() + " is stopped";
+    }
+
+//    public String isJustPassed () {
+//        return  getColor() + " " + getMake()   ;
+//    }
+
+    public String isJustPassed () {
+        return  "The " + getColor() + " " + getMake() + " just passed the "  ;
+    }
+
+} // end Car class
--- src/com/company/CarApp.java
+++ src/com/company/CarApp.java
@@ -3,14 +3,50 @@ package com.company;
 //if you want to print out value (sys.out) you have to set it first
 
 public class CarApp {
+    //main method
     public static void main(String[] args) {
         //this is how you create an instance of the car class
         Car car1 = new Car();
+        Car car2 = new Car();
 
         car1.setColor("red");
-        car1.setMake("Porsche");
+        car2.setColor("blue");
 
-        System.out.println(car1.getStarting());
-        System.out.println(car1.getAccelerating());
+        car1.setMake("Porsche");
+        car2.setMake("Jetta");
+
+        System.out.println(car1.isStarting());
+        System.out.println(car2.isStarting());
+
+        System.out.println(car1.isAccelerating());
+        System.out.println(car2.isAccelerating());
+
+        System.out.println(car2.isStopped());
+        System.out.println(car2.isPasses());
+
+        System.out.println(car1.isMph());
+        System.out.println(car1.isNotMoving());
+
+        //
+// now run methods toghether in a strng and create a space
+//        System.out.println("The " + car2.isJustPassed() + " just passed the " + car1.isJustPassed()
+        System.out.println(car2.isJustPassed() + car1.getColor() +  " " + car1.getMake());
+
+
+
+
+//        System.out.println(car1.getStarting());
+//        System.out.println(car2.getStarting());
+//
+//        System.out.println(car1.getAccelerating());
+//        System.out.println(car2.getAccelerating());
+//
+//        System.out.println(car2.getStopped());
+//        System.out.println(car2. getPasses());
+//
+//        System.out.println(car1.getMph());
+//        System.out.println(car2.getPasses());
+
+
     }
 }
